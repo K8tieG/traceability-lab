50d2b98f04b7120f42470c813e0843412803dc96
Katherine Gardner <katiegardner@Katherines-MacBook-Air.local>
2021-08-03 14:00:49 -0700

    added rollbar

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 // include and initialize the rollbar library with your access token
 var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
-  accessToken: '7232065577b64649b76d19abebd65cad',
+  accessToken: 'd4b737bb9cf449b9a94365bcfabe94f4',
   captureUncaught: true,
   captureUnhandledRejections: true
 });
@@ -19,8 +19,14 @@ app.use(express.json());
 //student data
 const pet = ['Alfalfa', 'Kitty', 'Tucker']
 
+
+//bad endpoint for testing
+// app.get('/', function(req, res){
+//   res.path.join.(--dirnamee)
+// })
+
 //endpoints
-app.get('/', function(Req, res){
+app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
@@ -37,13 +43,13 @@ app.post('/api/pets', function(req, res){
         if (index === -1 && name !== "") {
           pets.push(name);
           rollbar.info('A new pet is added!')
-          res.status(200).send(students);
+          res.status(200).send(pets);
         } else if (name === "") {
-            rollbar.error('Someone tried to enter a blank pet name')
+            rollbar.critical('Someone tried to enter a blank pet name')
           res.status(400).send("must provide a name");
 
         } else {
-            rollbar.error('Someone tried tp enter a duplicate pet name')
+            rollbar.warning('Someone tried tp enter a duplicate pet name')
           res.status(400).send("that pet already exists");
         }
       } catch (err) {
